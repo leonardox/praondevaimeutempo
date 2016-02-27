5088fe71c006a2571ea4564f058cb3e2ee4f9eb1
Leonardo.Cordeiro <leo.cc14@gmail.com>
2016-02-27 01:34:26 -0300

    aplicacao

--- praondevaimeutempo/settings.py
+++ praondevaimeutempo/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(BASE_DIR, 'aplicacao')
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'praondevaimeutempo.urls'
 TEMPLATES = (
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(APP_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- praondevaimeutempo/urls.py
+++ praondevaimeutempo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from aplicacao import views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 ]
