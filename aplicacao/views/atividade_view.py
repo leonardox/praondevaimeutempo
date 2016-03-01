dc64ad1459b69968c16f53bd1543428371e4283d
Leonardo.Cordeiro <leo.cc14@gmail.com>
2016-03-01 20:44:07 -0300

    filtrando atividades pela semana atual

--- aplicacao/views/atividade_view.py
+++ aplicacao/views/atividade_view.py
@@ -5,11 +5,7 @@ from django.views.generic import TemplateView, FormView
 
 from aplicacao.forms import FormAtividade
 from aplicacao.models import Atividade
-from django.utils.dateformat import DateFormat
-from django.utils.formats import get_format
-from django.template.defaulttags import register
 
-import datetime
 
 def index(request):
     return render_to_response('index.html')
@@ -19,33 +15,17 @@ class ListaAtividades(TemplateView):
     """
     Esta classe lista todas as atividades cadastradas.
     """
-    @register.filter
-    def get_item(dictionary, key):
-        return dictionary.get(key)
-
     template_name = 'dashboard.html'
-    one_day = datetime.timedelta(days=1)
-    def get_week(self, date):
-          day_idx = (date.weekday() + 1) % 7  # turn sunday into 0, monday into 1, etc.
-          sunday = date - datetime.timedelta(days=day_idx)
-          date = sunday
-          for n in xrange(7):
-            yield date
-            date += self.one_day
 
     def get(self, request, *args, **kwargs):
-        current_week = [d.isoformat() for d in self.get_week(datetime.datetime.now().date())]
-        converted = list(map(DateFormat, current_week))
-        print current_week
-        lista_atividades_semana = Atividade.objects.filter(data='2016-03-01')
-        print lista_atividades_semana
-        lista_atividades = Atividade.objects.all().order_by('-data')
-        page_data = {
-            'lista_atividades' : lista_atividades,
-            'lista_atividades_semana' : lista_atividades_semana
-        }
-        context = self.get_context_data(page_data=page_data)
-        return self.render_to_response(page_data)
+
+        date = datetime.date.today()
+        start_week = date - datetime.timedelta(date.weekday()+1)
+        end_week = start_week + datetime.timedelta(6)
+        lista_atividades = Atividade.objects.filter(data__range=[start_week, end_week])
+
+        context = self.get_context_data(lista_atividades=lista_atividades)
+        return self.render_to_response(context)
 
 
 class AdicionarAtividade(FormView):
