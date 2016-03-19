6eff3e879c5148d01a6118be8c0f0ccb1d535e48
Elyervesson de Souza Pereira <elyervesson@gmail.com>
2016-03-19 16:54:19 -0300

    Adição de testes para as funcionalidades de exibir apenas a categoria desejada

--- Testes/teste_adicionar_atividade.py
+++ Testes/teste_adicionar_atividade.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 from Testes import utils
 from aplicacao.models import Atividade
+from aplicacao.views import atividade_view
 
 
 class TesteAdicionarAtividade(TestCase):
@@ -39,6 +40,38 @@ class TesteAdicionarAtividade(TestCase):
         except IntegrityError:
             pass
 
-#class TesteVisualizarRelatorioCategoria(TestCase):
+class TesteVisualizarRelatorioCategoria(TestCase):
+    """
+    Esta classe realiza os testes relacionados com a visualização de atividades por uma categoria especifica
+    """
+    def teste_visualizar_atividade_lazer(self):
+        usuario = utils.criar_usuario()
 
-    #def teste_visualizar
+        atividade01 = Atividade(nome="Teste_01", tempo_investido=1, categoria='Lazer',
+                              user=usuario)
+
+        atividade02 = Atividade(nome="Teste_02", tempo_investido=2, categoria='Lazer',
+                              user=usuario)
+        atividade01.save()
+        atividade02.save()
+
+        atividade_db = Atividade.objects.filter(nome='Teste_01', user=usuario).values()[0]
+
+        atv_view = atividade_view._get_atividades_por_categoria(usuario.user_id, 'Lazer')
+
+        self.assertTrue(len(atv_view)==2)
+        self.assertEqual(atividade_db['nome'], atv_view[0].nome)
+
+        resumo_categ = atividade_view._get_resume_categ(atv_view)
+        resumo_atv01 = resumo_categ[0]
+
+        self.assertEquals(resumo_atv01[0], 'Teste_02')
+
+        utils.remover_atividade('Teste_01', usuario.id)
+        utils.remover_atividade('Teste_02', usuario.id)
+
+    def teste_atividade_categoria_nao_cadastrada(self):
+        usuario = utils.criar_usuario()
+
+        atv_view = atividade_view._get_atividades_por_categoria(usuario.user_id, 'Trabalho')
+        self.assertTrue(len(atv_view)==0)
