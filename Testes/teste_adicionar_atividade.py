b307cc6a5fe43e1fe13831756ac64c731fd67f98
Elyervesson de Souza Pereira <elyervesson@gmail.com>
2016-03-24 22:28:56 -0300

    Adição de testes da funcionalidade visualizar por tags

--- Testes/teste_adicionar_atividade.py
+++ Testes/teste_adicionar_atividade.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from Testes import utils
-from aplicacao.models import Atividade
+from aplicacao.models import Atividade, Tag
 from aplicacao.views import atividade_view
 
 
@@ -75,3 +75,45 @@ class TesteVisualizarRelatorioCategoria(TestCase):
 
         atv_view = atividade_view._get_atividades_por_categoria(usuario.user_id, 'Trabalho')
         self.assertTrue(len(atv_view)==0)
+
+class TesteVisualizarRelatorioPorTags(TestCase):
+    """
+    Esta classe realiza os testes relacionados com a visualização de atividades por tags especificas
+    """
+    def teste_usuario_sem_tag(self):
+        usuario = utils.criar_usuario()
+
+        atividade01 = Atividade(nome="Teste_01", tempo_investido=1, categoria='Lazer',
+                              user=usuario)
+        atividade01.save()
+
+        lista_de_tags = atividade_view.recupera_atividade_tag(usuario.user_id, 'Olar')#tag não exite no BD
+
+        self.assertTrue(len(lista_de_tags)==0)
+        self.assertEqual(lista_de_tags, [])
+
+    def teste_usuario_com_tag(self):
+        usuario = utils.criar_usuario()
+
+        atividade01 = Atividade(nome="Teste_01", tempo_investido=1, categoria='Lazer',
+                              user=usuario)
+
+        atividade02 = Atividade(nome="Teste_02", tempo_investido=2, categoria='Lazer',
+                              user=usuario)
+        atividade01.save()
+        atividade02.save()
+
+        atividade_view._salvar_tags('olar top', usuario, atividade01)
+        atividade_view._salvar_tags('show top', usuario, atividade02)
+
+        lista_de_atividades = atividade_view.recupera_atividade_tag(usuario.user_id, 'top')
+
+        self.assertTrue(len(lista_de_atividades)==2)#top esta cadastrado nas atividade 01 e 02
+        self.assertEqual(lista_de_atividades[0].nome, atividade01.nome)#lista_de_atividades[0] == atividade01
+
+        #recuperando todas as tags de um usuario
+        lista_tags = atividade_view.recupera_tags_usuario(usuario.user_id)#foram cadastradas 4 tags
+        self.assertTrue('olar' in lista_tags)
+        self.assertTrue('tagNaoCadastrada' not in lista_tags)
+
+
